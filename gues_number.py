c8512aedb1cc9daed7fa06d9da446a9dd36457a4
MahdiAjam <mahdiajam8754@gmail.com>
2024-05-26 22:35:04 +0330

    added gues number

---
+++ gues_number.py
@@ -0,0 +1,13 @@
+#second answer
+age = int(input('etner your age: '))
+
+if age < 7:
+    print('you are (A)')
+elif 7 <= age < 9:
+    print('you are (B)')
+elif 9 <= age < 12:
+    print('you are (C)')
+elif 12 <= age < 15:
+    print('you are (E)')
+else:
+    print('you are (E)')
